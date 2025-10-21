6b04339d8eb70e464ceeee0f2505432e7274dcf2
unknown <castrojhanfranco@gmail.com>
2025-10-21 17:39:39 -0500

    modelos lineales

---
+++ practica.R
@@ -0,0 +1,20 @@
+# mi primera practica con R
+x<- "hola"
+y <- "untrm"
+#f(x)=x+2^2+2xy
+paste(x,y)
+paste(x,y,sep="+")
+
+paste(x,y)
+library(tidyverse)
+#pipe
+a1 <- "estudiante"
+
+notas <- c(16,17,18,15)
+media <- mean(notas)
+
+#
+
+
+m <-c(16,17,18,15) %>% 
+  mean()
